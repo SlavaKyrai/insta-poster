a481c3f2a2808b39cb781806c556c362a6488fe4
viacheslav.kurai <viacheslav.kurai@eosda.com>
2022-05-06 18:11:53 +0300

    Add basic exception handling

--- web/apps/posters/tasks.py
+++ web/apps/posters/tasks.py
@@ -1,27 +1,30 @@
+from apps.posters.models import TelegramChannel
+from dj_imposter.celery import app
 from django.conf import settings
 from telegram.ext import Updater
 
-from apps.crawlers.models import Post
-from apps.posters.models import TelegramChannel
-from dj_imposter.celery import app
+from web.apps.posters.repositories import TelegramPosterService
 
 
 @app.task
 def post_to_telegram_chats():
     for channel in TelegramChannel.objects.filter(is_active=True):
-        post = Post.objects.filter(
-            subreddit__in=channel.reddit_sources.filter(is_active=True),
-            is_telegram_posted=False
-        ).order_by('-score').first()
+        post = TelegramPosterService.get_post_for_telegram_upload(channel)
         if post:
-            updater = Updater(
-                token=settings.TELEGRAM_TOKEN_ID,
-                use_context=True
-            )
-            updater.bot.send_photo(
-                chat_id=channel.chat_id,
-                photo=post.image_url,
-                caption=post.title
-            )
-            post.is_telegram_posted = True
-            post.save(update_fields={'is_telegram_posted'})
+            try:
+                updater = Updater(
+                    token=settings.TELEGRAM_TOKEN_ID,
+                    use_context=True
+                )
+                updater.bot.send_photo(
+                    chat_id=channel.chat_id,
+                    photo=post.image_url,
+                    caption=post.title
+                )
+            except Exception as e:
+                #TODO: add logger
+                print(e)
+            finally:
+                post.is_telegram_posted = True
+                post.save(update_fields={'is_telegram_posted'})
+
