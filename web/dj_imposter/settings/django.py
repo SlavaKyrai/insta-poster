f0bcc436ed756f4e7bf7249d9b36663a0f2e1163
viacheslav.kurai <viacheslav.kurai@eosda.com>
2022-05-06 17:35:21 +0300

    Update allowed hosts

--- web/dj_imposter/settings/django.py
+++ web/dj_imposter/settings/django.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://vps-9c9d8103.vps.ovh.net']
+ALLOWED_HOSTS = ['vps-9c9d8103.vps.ovh.net']
 
 
 # Application definition
