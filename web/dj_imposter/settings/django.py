73c8d6eda43f5902fdfeb3ac40842bc969fbb537
viacheslav.kurai <viacheslav.kurai@eosda.com>
2022-05-06 17:22:13 +0300

    Update config

--- web/dj_imposter/settings/django.py
+++ web/dj_imposter/settings/django.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://vps-9c9d8103.vps.ovh.net/']
 
 
 # Application definition
